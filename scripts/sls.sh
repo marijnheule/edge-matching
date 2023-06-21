2aeaa96ab186677b2977425ff74a8c6a9f82cfac
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2023-06-21 11:49:22 +0200

    sls parallel

--- scripts/sls.sh
+++ scripts/sls.sh
@@ -3,18 +3,20 @@
 INSTANCE=$1
 POSTFIX=$2
 TIMEOUT=3600
+DIR=/tmp/
 
 NAME=$(basename $INSTANCE)
 BASE=${NAME%.*}
 
 for SEED in `cat seeds`
 do
-  ../encoder/em-encode-$POSTFIX $INSTANCE $SEED > tmp-$SEED-$$.cnf
-  SIZE=`wc tmp-$SEED-$$.cnf | awk '{print $1}'`
+  ../encoder/em-encode-$POSTFIX $INSTANCE $SEED > $DIR/tmp-$SEED-$$.cnf
+  SIZE=`wc $DIR/tmp-$SEED-$$.cnf | awk '{print $1}'`
   SIZE=$(($SIZE-1))
-  cat tmp-$SEED-$$.cnf | awk '/p cnf/ {print "p cnf "$3" "'$SIZE'} / 0/ {print $0}' > cnf-$SEED-$$.cnf
-  mv cnf-$SEED-$$.cnf tmp-$SEED-$$.cnf
-  ubcsat -i tmp-$SEED-$$.cnf -alg ddfw -cutoff 100000000 | grep -e "TotalCPU" -e "   1 "
-#  ubcsat -i tmp-$SEED-$$.cnf -alg ddfw
+  cat $DIR/tmp-$SEED-$$.cnf | awk '/p cnf/ {print "p cnf "$3" "'$SIZE'} / 0/ {print $0}' > $DIR/cnf-$SEED-$$.cnf
+  mv $DIR/cnf-$SEED-$$.cnf $DIR/tmp-$SEED-$$.cnf
+  ubcsat -i $DIR/tmp-$SEED-$$.cnf -alg ddfw -cutoff 100000000 | grep -e "TotalCPU" -e "   1 " &
 done
-#wait
+wait
+
+rm $DIR/tmp-*-$$.cnf
